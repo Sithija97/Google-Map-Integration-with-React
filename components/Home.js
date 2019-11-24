8f95e8ef20825a1a5036b5c98c79552c0ec3a8f6
Sithija Shehara <nsithijashehara@gmail.com>
2019-11-24 16:50:11 +0530

    home screen updated

--- components/Home.js
+++ components/Home.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
+import * as firebase from 'firebase';
 
 export default class HomeScreen extends React.Component {
+  state = {
+    email: '',
+    displayName: '',
+  };
+
+  componentDidMount() {
+    const {email, displayName} = firebase.auth().currentUser;
+    this.setState({email, displayName});
+  }
+
+  signOutUser = () => {
+    firebase.auth().signOut();
+  };
   render() {
     return (
       <View style={styles.Container}>
-        <Text>Home</Text>
+        <Text>Hi {this.state.email}!</Text>
+
+        <TouchableOpacity style={{marginTop:32}} onPress ={this.signOutUser}>
+            <Text>Logout</Text>
+        </TouchableOpacity>
       </View>
     );
   }
--- components/Login.js
+++ components/Login.js
@@ -9,19 +9,39 @@ import {
 import * as firebase from 'firebase';
 
 export default class LoginScreen extends React.Component {
+  state = {
+    email: '',
+    password: '',
+    errorMessage: null,
+  };
+
+  handleLogin = () => {
+    const {email, password} = this.state;
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch(error => this.setState({errorMessage: error.message}));
+  };
+
   render() {
     return (
       <View style={styles.Container}>
         <Text style={styles.greeting}>{'Hello again.\nWelcome back.'}</Text>
 
         <View style={styles.errorMessage}>
-          <Text>Error</Text>
+          {this.state.errorMessage && (
+            <Text style={styles.error}>{this.state.errorMessage}</Text>
+          )}
         </View>
 
         <View style={styles.form}>
           <View>
             <Text style={styles.inputTitle}>Email</Text>
-            <TextInput style={styles.input} autoCapitalize="none"></TextInput>
+            <TextInput
+              style={styles.input}
+              autoCapitalize="none"
+              onChangeText={email => this.setState({email})}
+              value={this.state.email}></TextInput>
           </View>
 
           <View style={{marginTop: 32}}>
@@ -29,18 +49,23 @@ export default class LoginScreen extends React.Component {
             <TextInput
               style={styles.input}
               secureTextEntry
-              autoCapitalize="none"></TextInput>
+              autoCapitalize="none"
+              onChangeText={password => this.setState({password})}
+              value={this.state.password}></TextInput>
           </View>
         </View>
 
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.butoonText}>Signin</Text>
+        <TouchableOpacity style={styles.button} onPress={this.handleLogin}>
+          <Text style={styles.butoonText}>Sign in</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={{ alignSelf:"center", marginTop:32}}>
-            <Text style = {{color:"#414959", fontSize:13}}>
-                New to SocialApp? <Text style={{fontWeight:"500", color:"#5352ed"}}>Sign Up</Text> 
-            </Text>
+        <TouchableOpacity
+          style={{alignSelf: 'center', marginTop: 32}}
+          onPress={() => this.props.navigation.navigate('Register')}>
+          <Text style={{color: '#414959', fontSize: 13}}>
+            New to SocialApp?
+            <Text style={{fontWeight: '500', color: '#5352ed'}}>Sign Up</Text>
+          </Text>
         </TouchableOpacity>
       </View>
     );
@@ -58,38 +83,44 @@ const styles = StyleSheet.create({
     fontWeight: '400',
     textAlign: 'center',
   },
-  errorMessage:{
-      height:72,
-      alignItems:"center",
-      justifyContent:"center",
-      marginHorizontal:30
+  errorMessage: {
+    height: 72,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginHorizontal: 30,
   },
-  form:{
-      marginBottom:48,
-      marginHorizontal:30
+  form: {
+    marginBottom: 48,
+    marginHorizontal: 30,
   },
-  inputTitle:{
-      color:"#8A8F9E",
-      fontSize:10,
-      textTransform: "uppercase"
+  inputTitle: {
+    color: '#8A8F9E',
+    fontSize: 10,
+    textTransform: 'uppercase',
   },
-  input:{
-      borderBottomColor:"#8A8F9E",
-      borderBottomWidth: StyleSheet.hairlinewidth,
-      height:40,
-      fontSize:15,
-      color:"#161F3D"
+  input: {
+    borderBottomColor: '#57606f',
+    borderBottomWidth: StyleSheet.hairlinewidth,
+    height: 40,
+    fontSize: 15,
+    color: '#57606f',
   },
-  button:{
-      marginHorizontal:30,
-      backgroundColor:"#5352ed",
-      borderRadius:4,
-      height:45,
-      alignItems:"center",
-      justifyContent:"center",
+  button: {
+    marginHorizontal: 30,
+    backgroundColor: '#5352ed',
+    borderRadius: 4,
+    height: 45,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  butoonText:{
-      color:"#f1f2f6",
-      fontWeight:'800'
-  }
+  butoonText: {
+    color: '#f1f2f6',
+    fontWeight: '800',
+  },
+  error: {
+    color: '#ff4757',
+    fontSize: 13,
+    fontWeight: '600',
+    textAlign: 'center',
+  },
 });
