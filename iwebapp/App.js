6daa154583b9cf3ca45edeed07017ee2973a248f
Sithija Shehara <nsithijashehara@gmail.com>
2020-01-08 15:41:42 +0530

    tried a splash animation didint work

--- iwebapp/App.js
+++ iwebapp/App.js
@@ -1,29 +1,14 @@
 import React from "react"
 import { createSwitchNavigator, createAppContainer } from "react-navigation"
 
-//firebase initialization
-import firebase from "firebase";
-import firebaseConfig from "./config";
-firebase.initializeApp(firebaseConfig);
-
 //components
-import LoadingScreen from "./components/Loading";
-import LoginScreen from "./components/Login";
+
 import DashboardScreen from "./components/Dashboard";
 
 export default class App extends React.Component {
   render(){
     return(
-      <AppConatiner/>
+      <DashboardScreen/>
     );
   }
 }
-
-const AppSwitchNavigator = createSwitchNavigator({
-  Load:LoadingScreen,
-  Login:LoginScreen,
-  Dashboard:DashboardScreen
-
-});
-
-const AppConatiner = createAppContainer(AppSwitchNavigator);
--- iwebapp/components/Loading.js
+++ iwebapp/components/Loading.js
@@ -1,31 +1,12 @@
 import React from "react"
 import { StyleSheet, Text, View, Image, Button,ActivityIndicator } from "react-native"
-import firebase from "firebase";
+
 
 export default class LoadingScreen extends React.Component {
-
-    componentDidMount(){
-        this.checkIfLoggedIn();
-    }
-
-
-    checkIfLoggedIn = () =>{
-        firebase.auth().onAuthStateChanged(user =>
-        {
-            if(user)
-            {
-                this.props.navigation.navigate("DashboardScreen");
-            }
-            else{
-                this.props.navigation.navigate("LoginScreen");
-
-            }
-        })
-        }
   render(){
     return(
       <View style={styles.container}>
-        <ActivityIndicator size="large"/>
+        <Text>Loading..</Text>
       </View>
     );
   }
