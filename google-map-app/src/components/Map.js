b864b7243402bd2bcdf595daf99ec9eeac0fed7c
Sithija Shehara <nsithijashehara@gmail.com>
2021-08-08 23:51:55 +0530

    Search & Map functionality added

--- google-map-app/src/components/Map.js
+++ google-map-app/src/components/Map.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useRef, useState } from "react";
 import {
   GoogleMap,
   useLoadScript,
@@ -8,6 +8,7 @@ import {
 } from "@react-google-maps/api";
 import { formatRelative } from "date-fns";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import Searchbar from "./Searchbar";
 
 const libraries = ["places"];
 const mapContainerStyle = {
@@ -21,6 +22,30 @@ const center = {
 
 function MapView() {
   const [markers, setMarkers] = useState([]);
+  const [selected, setSelected] = useState(null);
+
+  const onMapClick = useCallback((event) => {
+    console.log(event.latLng.lat());
+    console.log(event.latLng.lng());
+    setMarkers((current) => [
+      ...current,
+      {
+        lat: event.latLng.lat(),
+        lng: event.latLng.lng(),
+        time: new Date(),
+      },
+    ]);
+  });
+
+  const mapRef = useRef();
+  const onMapLoad = useCallback((map) => {
+    mapRef.current = map;
+  }, []);
+
+  const panTo = useCallback(({ lat, lng }) => {
+    mapRef.current.panTo({ lat, lng });
+    mapRef.current.setZoom(12);
+  }, []);
 
   const { isLoaded, LoadError } = useJsApiLoader({
     id: "google-map-script",
@@ -33,29 +58,37 @@ function MapView() {
 
   return (
     <div>
+      <Searchbar panTo={panTo} />
       <GoogleMap
         mapContainerStyle={mapContainerStyle}
         zoom={8}
         center={center}
-        onClick={(event) => {
-          console.log(event.latLng.lat());
-          console.log(event.latLng.lng());
-          setMarkers((current) => [
-            ...current,
-            {
-              lat: event.latLng.lat(),
-              lng: event.latLng.lng(),
-              time: new Date(),
-            },
-          ]);
-        }}
+        onClick={onMapClick}
+        onLoad={onMapLoad}
       >
         {markers.map((marker) => (
           <Marker
             key={marker.time.toISOString()}
             position={{ lat: marker.lat, lng: marker.lng }}
+            onClick={() => {
+              setSelected(marker);
+            }}
           />
         ))}
+
+        {selected ? (
+          <InfoWindow
+            position={{ lat: selected.lat, lng: selected.lng }}
+            onCloseClick={() => {
+              setSelected(null);
+            }}
+          >
+            <div>
+              <h2>Leopard Spotted ! 🐆 🐯 </h2>
+              <p>Spotted {formatRelative(selected.time, new Date())}</p>
+            </div>
+          </InfoWindow>
+        ) : null}
       </GoogleMap>
     </div>
   );
---
+++ google-map-app/src/components/Searchbar.js
@@ -0,0 +1,63 @@
+import React from "react";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+  ComboboxOptionText,
+} from "@reach/combobox";
+import "@reach/combobox/styles.css";
+
+function Searchbar({ panTo }) {
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+    clearSuggestions,
+  } = usePlacesAutocomplete({
+    requestOptions: {
+      location: { lat: () => 7.873054, lng: () => 80.771797 },
+      radius: 200 * 1000,
+    },
+  });
+  return (
+    <div className="search">
+      <Combobox
+        onSelect={async (address) => {
+          setValue(address, false);
+          clearSuggestions();
+          try {
+            const results = await getGeocode({ address });
+            const { lat, lng } = await getLatLng(results[0]);
+            panTo({ lat, lng });
+          } catch (error) {
+            console.log("error !", error);
+          }
+        }}
+      >
+        <ComboboxInput
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+          disabled={!ready}
+          placeholder="Enter a place"
+        />
+        <ComboboxPopover>
+          {status === "OK" &&
+            data.map(({ place_id, description }) => (
+              <ComboboxOption key={place_id} value={description} />
+            ))}
+        </ComboboxPopover>
+      </Combobox>
+    </div>
+  );
+}
+
+export default Searchbar;
